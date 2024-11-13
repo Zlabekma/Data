389e511deda43a9086877590b5b0820ab27089fb
LAPTOP-58JCMANU\fel <zlabemar@fel.cvut.cz>
2024-11-13 13:34:39 +0100

    port correction

--- Bowling_dash_2.py
+++ Bowling_dash_2.py
@@ -357,5 +357,5 @@ def toggle_player_dropdown(filter_value):
     else:
         return {'display': 'none'}
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
